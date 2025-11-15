d52fe0ced02565a1e87d0deab1513160391b17aa
Esther Yu <estyuyui@gmail.com>
2025-11-15 15:13:13 -0500

    fixed login

--- FlowState/src/App.tsx
+++ FlowState/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
-
+import Login from './pages/Login.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
-
 export default App
